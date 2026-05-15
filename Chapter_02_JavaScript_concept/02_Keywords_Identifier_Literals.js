2cc533d27286fffffc244134f740f8b18abbdf7e
Mohit Anand Godiyal <mohit.godiyal@tollring.com>
2026-05-15 18:31:50 +0530

    Add Chapter 2 JavaScript concepts

---
+++ Chapter_02_JavaScript_concept/02_Keywords_Identifier_Literals.js
@@ -0,0 +1,26 @@
+var v = 10;
+
+//var is a keyword, because it is a reserved word in the programming language.
+//v is identifer or variable name
+// = is operator
+//10 is literal or value
+// and this whole statement is variable , it is like a container which contains everything.
+
+/* Example of Keywords: break, case, catch, class, const, continue, debugger,
+ default, delete, do, else, export, extends, finally, for, function, if, import,
+ in, instanceof, new, return, super, switch, this, throw, try, typeof, var,
+ void, while, with, yield, implements, interface, let, package, private, protected,
+ public, static, enum, await, as, async, from, get, of, set, target, true, false, null,
+ undefined */
+
+
+//  Rules of Identifers
+/* --
+• Must begin with a letter (a–z, A–Z), underscore (_), or dollar sign ($).
+• Subsequent characters may also include digits (0–9).
+• Cannot start with a digit.
+• Cannot be a reserved keyword.
+• Are case-sensitive.
+• May contain Unicode letters and Unicode escape sequences.
+• Cannot contain spaces, hyphens, or special characters (except _ and $).
+*/
---
+++ Chapter_02_JavaScript_concept/04_Functions_Basic_Concept.js
@@ -0,0 +1,16 @@
+/*
+## Function 
+Reusable code that can be called multiple times.
+
+1. Definition of function
+2. Calling of function
+*/
+
+// 1. Define of function
+function greet() {
+    console.log("Hi, How are you?");
+}
+
+// 2. Calling of the function
+greet();
+greet();
