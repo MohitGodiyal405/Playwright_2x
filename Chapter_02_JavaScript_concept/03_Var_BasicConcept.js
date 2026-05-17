78a1e2b263fadcfe63519868fba97cac6611dd6f
Mohit Anand Godiyal <mohit.godiyal@tollring.com>
2026-05-17 13:29:33 +0530

    Update var basics with function scoping examples

--- Chapter_02_JavaScript_concept/03_Var_BasicConcept.js
+++ Chapter_02_JavaScript_concept/03_Var_BasicConcept.js
@@ -14,3 +14,62 @@ var v = 10;
 var browser = "chrome";
 var browser = "firefox"; // redeclaration allowed
 browser = "edge"; // reassignment allowed
+
+// var is function scoped
+
+var a = 20;
+console.log(a);
+
+if (true) {
+    var a = 30;
+    console.log(a);
+}
+
+/* Function Scoped Meaning in JavaScript
+When a variable is function-scoped, it means:
+The variable exists everywhere inside the function where it was created.
+var is function-scoped. */
+
+function test() {
+    var age = 25;
+
+    if (true) {
+        console.log(age); // ✅ accessible
+    }
+
+    console.log(age); // ✅ accessible
+}
+test();
+
+/*Because var belongs to the whole function:
+    function test() {
+   ← var lives in entire area
+}
+It does NOT care about { } blocks. */
+
+//ANOTHER EXAMPLE:
+
+function demo() {
+
+    if (true) {
+        var message = "hello";
+    }
+
+    console.log(message); // ✅ works
+}
+
+demo();
+
+//Even though message was inside if, it still works outside because var is function-scoped.
+
+
+
+//But Outside Function
+function demo() {
+    var x = 10;
+}
+console.log(x); // ❌ Error
+
+// Because the variable exists only inside that function.
+
+//Function scoped = Variable can be used anywhere inside the function, but not outside the function.
